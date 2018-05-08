f1a0587ca3fc78b023bb335903a145fdd3764eb1
alenamandrika <Alenenok0@ukr.net>
2018-05-08 13:19:46 +0300

    add contact-list

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Contact from './components/contactCard/contactCard'
+import AniLorak from './assets/images/AniLorak.jpeg'
+import Anja from './assets/images/Anja.jpeg'
+import Natasha from './assets/images/Natasha.jpeg'
+import TinaCarol from './assets/images/TinaCarol.jpeg'
+
+let CONTACTS = [
+  {
+    id: 1,
+    name: 'Karolina',
+    phoneNumber: '+380966666666',
+    email: 'AniLorak@gmail.com',
+    image: AniLorak,
+    alt: 'AniLorak'
+  }, {
+    id: 2,
+    name: 'Cedokova',
+    phoneNumber: '+380966344466',
+    email: 'Anja@gmail.com',
+    image: Anja,
+    alt: 'Anja'
+  }, {
+    id: 3,
+    name: 'Natasha',
+    phoneNumber: '+380976654433',
+    email: 'Natasha@gmail.com',
+    image: Natasha,
+    alt: 'Natasha'
+  }, {
+    id: 4,
+    name: 'Tina',
+    phoneNumber: '+380456784935',
+    email: 'TinaCarol@gmail.com',
+    image: TinaCarol,
+    alt: 'TinaCarol'
+  }
+];
+
+class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      displayedContacts: CONTACTS,
+
+      isVisible: true,
+      isHidden: false,
+
+      value: 0
+    }
+
+    this.handleSearch = this.handleSearch.bind(this)
+    this.addContactList = this.addContactList.bind(this)
+    this.deleteContactList = this.deleteContactList.bind(this)
+    this.handleDecrement = this.handleDecrement.bind(this)
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
+
+  handleSearch (event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let displayedContacts = CONTACTS.filter(function(el) {
+      let searchValue = el.name.toLowerCase();
+      console.log(searchValue)
+      return searchValue.indexOf(searchQuery) !== -1;
+    });
+    console.log(displayedContacts)
+    this.setState({
+      displayedContacts: displayedContacts
+    });
+  }
+
+  addContactList () {
+    this.setState ({
+      isVisible: false,
+      isHidden: true,
+    });
+  }
+
+  deleteContactList () {
+    this.setState ({
+      isVisible: true,
+      isHidden: false,
+    });
+  }
+
+  handleDecrement () {
+    console.log ('value before', this.state.value)
+    this.setState ({
+      value: this.state.value - 1,
+    }, () => {
+      console.log ('value after1', this.state.value)//выполнится, когда состояние поменяется
+    });
+    console.log ('value after2', this.state.value)
+  }
+
+  handleIncrement () {
+    this.setState ({
+      value: this.state.value + 1
+    });
+  }
+
+  render() {
+    return (
+      <div className="contacts">
+        <div className='counter-block'>
+          <button onClick={this.handleDecrement} className='btn-counter'>-</button>
+          <h1>{this.state.value}</h1>
+          <button onClick={this.handleIncrement} className='btn-counter'>+</button>
+        </div>
+        <div>
+          <button className='btnAddContactList' onClick={this.addContactList} hidden={!this.state.isVisible}>add contact list</button>
+        </div>
+        <div className='block-contact' hidden={!this.state.isHidden}>
+          <input type="text" placeholder="Search..." className="search-field" onChange={this.handleSearch} />
+          <ul className="contacts-list">
+            {
+              this.state.displayedContacts.map(function(el) {
+                return <Contact
+                  key={el.id}
+                  name={el.name}
+                  phoneNumber={el.phoneNumber}
+                  email={el.email}
+                  image={el.image}
+                  alt={el.alt}
+                />;
+              })
+            }
+          </ul>
+          <button className='btnDeleteContactList' onClick={this.deleteContactList} hidden={this.state.isVisible}>delete contact list</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contactCard/contactCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render () {
+    return (
+      <li className="contact">
+        <img className="contact-image" src={this.props.image} alt={this.props.alt} width="60px" height="60px" />
+        <div className="contact-info">
+          <div className="contact-name"> {this.props.name} </div>
+          <div className="contact-number"> {this.props.phoneNumber} </div>
+          <div className="contact-number"> {this.props.email} </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Contact
